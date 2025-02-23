5a29525d5b00cc661f8c8b689ce63a810345588f
Andrew Shum <andrew.shum068@gmail.com>
2025-02-23 18:37:38 -0500

    chunky

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-import os
+import os, time
 from flask import Flask, jsonify, request
 from supabase import create_client, Client
 
@@ -59,10 +59,32 @@ def get_user_sessions(user_id):
             mapping[key] = value
         dict_mapping[category] = mapping    
     
-    events_response = supabase.table(TABLE_NAME).select("*").eq("amplitude_id", amplitude_id).order("event_time", desc=False).execute()
-    
+    batch_size = 10000
+    offset = 0
+    events_response = []
+    while True:
+        try:
+            events_batch = (
+                supabase.table(TABLE_NAME)
+                .select("*")
+                .order("event_time", desc=False)
+                .eq("amplitude_id", amplitude_id)
+                .range(offset, offset + batch_size - 1)
+                .execute()
+            )
+            if not events_batch.data:
+                break
+            events_response.extend(events_batch.data)
+            offset += batch_size
+            time.sleep(0.8)
+            print(offset)
+        except Exception as batch_error:
+            print(batch_error, "continuing ...")
+            time.sleep(5)
+    print(f"processed between {offset-batch_size} and {offset} rows")
+
     events = []
-    for event in events_response.data:
+    for event in events_response:
         result = {}
         for category in categories:
             result[category] = event[category]
@@ -133,39 +155,6 @@ def get_users_sessions_range(start_id, end_id):
     
     return jsonify(results)
 
-# def get_dict_values(): in progress
-#     dicts = [
-#         "event_type",
-#         "region",
-#         "country",
-#         "language",
-#         "device_family",
-#         "device_type",
-#         "os_name",
-#     ]
-
-#     dict_mapping = {}
-#     for category in dicts:
-#         dict_response = supabase.table(category).select(f"{category}, dict_{category}").execute()
-#         mapping = {}
-#         for row in dict_response.data:
-#             key = row[category]
-#             value = row[f"dict_{category}"]
-#             mapping[key] = value
-#         dict_mapping[category] = mapping  
-
-#     events = []
-#     for event in events_response.data:
-#         result = {}
-#         for category in categories:
-#             result[category] = event[category]
-        
-#         for category in dicts:
-#             result[f"dict_{category}"] = dict_mapping.get(category).get(result[category])
-
-#         events.append(result)
-
-
 profile = {}
 @app.route("/set_profile", methods=["POST"])
 def set_profile():
@@ -197,13 +186,168 @@ def set_profile():
 def get_profile():
     return jsonify(profile)
 
-# @app.route("/create_session", methods=["GET"]) in progress
-# def create_user_session():
-#     events = []
-#     for i in range(5):
-#         events.append(request.form.get(f"event_{i}"))
-#     session = profile
-#     return
+@app.route("/create_session", methods=["GET"])
+def create_user_session():
+    dict_et_mapping = {}
+    dict_response = supabase.table("event_type").select("event_type", "dict_event_type").execute()
+    for row in dict_response.data:
+        key = row["event_type"]
+        value = row["dict_event_type"]
+        dict_et_mapping[key] = value
+
+    events = []        
+       
+    for i in range(5):
+        event_json = {}
+        event_json["dict_next_et"] = None
+        event_json["next_et"] = None
+        event_json["time_since_last"] = None
+        event_json["time_to_next_event"] = None
+        for j in range(1, 5):
+            event_json[f"dict_pet{j}"] = None
+            event_json[f"prev_{j}_event_type"] = None
+            event_json[f"time_since_last_{j}"] = None
+
+        event_type = request.form.get(f"event_{i+1}")
+        event_json["event_type"] = event_type
+        event_json["dict_event_type"] = dict_et_mapping.get(event_type)
+
+        if i > 0:
+            event_json["time_since_last"] = event_json["event_time"] - events[i-1]["event_time"]
+
+
+
+
+@app.route("/model/search/user_chunk/<int:user_id>/<int:chunk>", methods=["GET"])
+def get_user_chunk(user_id, chunk):
+    categories = [
+        "amplitude_id",
+        "app", 
+        "region", 
+        "country",
+        "language",
+        "device_family",
+        "device_type",
+        "os_name",
+        "session_id",
+        "event_type",
+        "event_time",
+        "platform",
+    ]
+    
+    dicts = [
+        "event_type",
+        "region",
+        "country",
+        "language",
+        "device_family",
+        "device_type",
+        "os_name",
+    ]
+
+    user_response = supabase.table("user_table").select("amplitude_id, average_session_time, total_session_time, user_retention_30").eq("user_id", user_id).execute()
+    user_response = user_response.data[0]
+    amplitude_id = user_response["amplitude_id"]
+
+    dict_mapping = {}
+    for category in dicts:
+        dict_response = supabase.table(category).select(f"{category}, dict_{category}").execute()
+        mapping = {}
+        for row in dict_response.data:
+            key = row[category]
+            value = row[f"dict_{category}"]
+            mapping[key] = value
+        dict_mapping[category] = mapping    
+    
+    batch_size = 10000
+    offset = chunk * batch_size
+    events_response = []
+    
+    
+    events_batch = (
+        supabase.table(TABLE_NAME)
+        .select("*")
+        .order("event_time", desc=False)
+        .eq("amplitude_id", amplitude_id)
+        .range(offset, offset + batch_size - 1)
+        .execute()
+    )
+    if not events_batch.data:
+        print(f"no chunk at rows {offset} to {offset + batch_size}")
+        return f"no chunk at rows {offset} to {offset + batch_size}", 400
+    events_response.extend(events_batch.data)
+    
+    print(f"processed rows {offset} to {offset + batch_size}")
+
+    events = []
+    for event in events_response:
+        result = {}
+        for category in categories:
+            result[category] = event[category]
+        
+        for category in dicts:
+            result[f"dict_{category}"] = dict_mapping.get(category).get(result[category])
+
+        result["average_session_time"] = user_response["average_session_time"]
+        result["total_session_time"] = user_response["total_session_time"]
+        result["user_retention_30"] = user_response["user_retention_30"]
+
+        events.append(result)
+
+    for event in events:
+        if "." in event["event_time"]:
+            date_part, ms_part = event["event_time"].split(".")
+            ms_part = (ms_part + "000000")[:6]  
+            event["event_time"] = datetime.fromisoformat(f"{date_part}.{ms_part}")
+        else:
+            event["event_time"] = datetime.fromisoformat(event["event_time"])
+
+
+    for i, event in enumerate(events):
+        if i > 0:
+            event["time_since_last"] = (event["event_time"] - events[i-1]["event_time"]).total_seconds()
+            if event["time_since_last"] < 0:
+                event["time_since_last"] = None
+        else:
+            event["time_since_last"] = None
+        
+        if i < len(events) - 1:
+            event["time_to_next_event"] = (events[i+1]["event_time"] - event["event_time"]).total_seconds()
+            if event["time_to_next_event"] < 0:
+                event["time_to_next_event"] = None
+        else:
+            event["time_to_next_event"] = None
+
+        for j in range(1, 5):
+            if i - j >= 0:
+                event[f"prev_{j}_event_type"] = events[i - j]["event_type"]
+                event[f"dict_pet{j}"] = events[i - j]["dict_event_type"]
+                event[f"time_since_last_{j}"] = (event["event_time"] - events[i-j]["event_time"]).total_seconds()
+                if event[f"time_since_last_{j}"] < 0:
+                    event[f"time_since_last_{j}"] = None
+            else:
+                event[f"prev_{j}_event_type"] = ""
+                event[f"dict_pet{j}"] = None
+                event[f"time_since_last_{j}"] = None
+
+        if i < len(events) - 1:
+            event["next_et"] = events[i + 1]["event_type"]
+            event["dict_next_et"] = events[i + 1]["dict_event_type"]
+        else:
+            event["next_et"] = ""
+            event["dict_next_et"] = None
+
+    for event in events:
+        event["event_time"] = event["event_time"].isoformat()
+    
+    return jsonify(events)
+    
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
